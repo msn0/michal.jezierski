8850886b324fbcf486a12f0e09e69f28d6a63b1b
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-16 15:03:38 +0100

    :bear:

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '@context': 'http://schema.org',
+    '@type': 'Person',
+    name: 'Michał Jezierski',
+    url: 'https://github.com/msn0',
+    email: 'ambinanitelo@gmail.com'
+};

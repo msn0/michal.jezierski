c57afb9244faf6564573d1d05c9df501ad366ca1
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-17 10:29:26 +0100

    :bear:

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 module.exports = {
-    '@context': 'http://schema.org',
-    '@type': 'Person',
-    name: 'Michał Jezierski',
-    url: 'https://github.com/msn0',
-    email: 'ambinanitelo@gmail.com'
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "Michał Jezierski",
+    "url": "https://github.com/msn0",
+    "email": "ambinanitelo@gmail.com",
+    "jobTitle": "Software Engineer",
+    "worksFor": {
+        "@type": "Organization",
+        "url": "https://spartez.com",
+        "name": "Spartez"
+    }
 };

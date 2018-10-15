28be2b0940b440a92f5a53c41dfdef8605462a0c
Michał Jezierski <ambinanitelo@gmail.com>
2018-10-15 15:43:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ module.exports = {
     "name": "Michał Jezierski",
     "url": "https://github.com/msn0",
     "email": "ambinanitelo@gmail.com",
+    "birthDate": "20-06-1983",
+    "telephone": "+48666610106",
     "jobTitle": "Software Engineer",
     "worksFor": {
         "@type": "Organization",
-        "url": "https://spartez.com",
-        "name": "Spartez"
+        "url": "https://allegro.pl",
+        "name": "allegro.pl"
     }
 };
